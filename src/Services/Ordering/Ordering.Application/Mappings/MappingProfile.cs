74ee3c50fd0cc350f75fdc10d7be0498d89c1a25
vmyana <vamshi-krishna.myana@capgemini.com>
2023-02-13 22:17:09 +0530

    code changes

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
